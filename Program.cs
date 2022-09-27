ebe3e9613e4c20b20de0b4d31e117ffccd11051f
Connie Parker <connie.parker@payroc.com>
2022-09-27 10:36:01 +0100

    Changed to .NET Framework for superior resx support

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-// === Utility wireframe ===
-// 1. Input a csproj file
-// 2. Load all resx files from that csproj
-// 3. Find duplicates
-// 4. Merge duplicates into single keypair (store duplicates in memory for next part)
-// 5. Iterate over all files in csproj *apart from resx*
-// 6. Find and replace merged keypairs in string
-// 7. Save back down
-// 8. Done
+namespace resource_merger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
